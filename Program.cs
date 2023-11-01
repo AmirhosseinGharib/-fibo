57f4d24556de08917e65dafd86952fbbc307674e
AmirhosseinGharib <149072424+AmirhosseinGharib@users.noreply.github.com>
2023-11-01 14:32:30 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("sfdkjdsfjksbkfs?");
+int n =Convert.ToInt32(Console.ReadLine());
+int a = 0;
+int b = 1;
+for (int i = 0; i < n; i++)
+{
+    int temp = a; a = b; b = temp + b;
+}
+return a;
+
+static void Main()
+{
+
+
+
+    for (int i = 0; i < 47; i++)
+    {
+        Console.WriteLine();
+    
+    Console.ReadLine();
+} } 
+
+
+
+
+
